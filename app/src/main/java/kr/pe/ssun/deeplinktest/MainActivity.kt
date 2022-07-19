3e9698019fb6a4e019806f968bc25a826c000a70
sunchulbaek <x1210x@gmail.com>
2022-07-19 14:05:59 +0900

    init

--- app/src/main/java/kr/pe/ssun/deeplinktest/MainActivity.kt
+++ app/src/main/java/kr/pe/ssun/deeplinktest/MainActivity.kt
@@ -1,15 +1,21 @@
 package kr.pe.ssun.deeplinktest
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import kr.pe.ssun.deeplinktest.ui.theme.DeepLinkTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +23,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DeepLinkTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Scaffold(topBar = { TopAppBar() {
+                    Text(text = "딥링크 테스트")
+                }}) { paddingValues ->
+                    MainContent(paddingValues) { url ->
+                        Toast.makeText(this, url, Toast.LENGTH_SHORT).show()
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                        startActivity(intent)
+                    }
                 }
             }
         }
@@ -30,14 +38,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainContent(paddingValues: PaddingValues, onClick: (String)->Unit) {
+    Column(modifier = Modifier.padding(paddingValues)) {
+        val scheme = rememberSaveable { mutableStateOf("fanpa") }
+        Row1("scheme", scheme)
+        val host = rememberSaveable { mutableStateOf("open") }
+        Row1("host", host)
+        val path = rememberSaveable { mutableStateOf("") }
+        Row1("path", path)
+
+        val query1 = rememberSaveable { mutableStateOf("url") }
+        val param1 = rememberSaveable { mutableStateOf("https://www.naver.com") }
+        Row2(query1, param1)
+        
+        val query2 = rememberSaveable { mutableStateOf("title") }
+        val param2 = rememberSaveable { mutableStateOf("네이버") }
+        Row2(query2, param2)
+
+        // 동작
+        val url = makeUrl(scheme.value, host.value, path.value, query1.value, param1.value, query2.value, param2.value)
+        Text(text = url)
+        Button(onClick = { onClick.invoke(url) }) {
+            Text(
+                text = "열기",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+fun makeUrl(scheme: String, host: String?, path: String?, query1: String?, param1: String?, query2: String?, param2: String?): String {
+    return "$scheme://${host ?: ""}${if(path.isNullOrEmpty()) "" else "/"}${path ?: ""}${if (query1.isNullOrEmpty()) "" else "?"}$query1=$param1${if (query2.isNullOrEmpty()) "" else "&"}$query2=$param2"
+}
+
 @Composable
-fun DefaultPreview() {
-    DeepLinkTestTheme {
-        Greeting("Android")
+fun Row1(label: String, state: MutableState<String>) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text("$label : ")
+        TextField(value = state.value, onValueChange = {
+            state.value = it
+        })
+    }
+}
+
+@Composable
+fun Row2(state1: MutableState<String>, state2: MutableState<String>) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        TextField(value = state1.value, label = { Text("query") }, onValueChange = {
+            state1.value = it
+        }, modifier = Modifier.width(80.dp))
+        Text(":")
+        TextField(value = state2.value, label = { Text("param")}, onValueChange = {
+            state2.value = it
+        })
     }
 }
